31f8c583a0a2bde10bfc89e30fa2e2fc4303d900
Zack-szh <919zack@gmail.com>
2025-10-24 14:59:08 -0400

    dataloader and line detection

--- main.py
+++ main.py
@@ -15,6 +15,53 @@ def load_img(img_path):
     
     return img
 
-def load_img_from_folder(folder_path): 
+
+def load_img_from_folder(folder_path, batch_size = 10): 
     """Loads images from folder into a list """
-    
+
+    supported_format = ('jpg', 'jpeg', 'png')
+    data = []
+    count = 0
+
+    for f in os.listdir(folder_path): 
+        if f.endswith(supported_format) and count < batch_size: 
+            img_path = os.path.join(folder_path, f)
+            img = load_img(img_path)
+            data.append(img)
+            count += 1
+        else: 
+            break
+
+    return data
+
+
+def detect_line(img, canny_thres1=50, 
+             canny_thres2=150, 
+             rho=1, 
+             theta=np.pi/180, 
+             hough_thres=80, 
+             min_line_len=30, 
+             max_line_gap=10
+             ): 
+    """The line detection pipeline"""
+
+    # the images are already loaded in as grayscale
+
+    # step 1: canny edge detections
+    edges = cv2.Canny(img, canny_thres1, canny_thres2)
+
+    # step 2: detect lines
+    lines = cv2.HoughLinesP(edges, rho, theta, hough_thres, minLineLength=min_line_len, maxLineGap=max_line_gap)
+
+    if lines is None:
+        return []
+
+    return [tuple(line[0]) for line in lines]  # flatten
+
+
+def draw_lines(img, lines, color=(0, 0, 255), thickness=2): 
+    img_out = img.copy()
+    for (x1, y1, x2, y2) in lines: 
+        cv2.line(img_out, (x1, y1), (x2, y2), color, thickness)
+
+    return img_out
